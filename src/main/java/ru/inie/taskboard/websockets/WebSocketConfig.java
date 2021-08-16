e13290100cda5ecbb4dda9a7aff0f2b710467457
inie94 <ananian94@gmail.com>
2021-08-17 02:00:48 +0300

    Realized:    Edit profile complete on 50%;

---
+++ src/main/java/ru/inie/taskboard/websockets/WebSocketConfig.java
@@ -0,0 +1,9 @@
+package ru.inie.taskboard.websockets;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+
+//@Configuration
+//@EnableWebSocketMessageBroker
+public class WebSocketConfig {
+}
